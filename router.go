dbd9e33eaf0479721049f36de149af07b9db0465
aniaan <fjn.aniaan@gmail.com>
2022-10-19 19:27:13 +0800

    fix(insert): insert

--- router.go
+++ router.go
@@ -1,8 +1,10 @@
 package artrouter
 
 import (
+	"fmt"
 	"net/http"
 	"regexp"
+	"sort"
 	"strings"
 )
 
@@ -65,6 +67,12 @@ type endpoints map[methodType]*endpoint
 
 type nodes []*node
 
+// Sort the list of nodes by label
+func (ns nodes) Sort()              { sort.Sort(ns); ns.tailSort() }
+func (ns nodes) Len() int           { return len(ns) }
+func (ns nodes) Swap(i, j int)      { ns[i], ns[j] = ns[j], ns[i] }
+func (ns nodes) Less(i, j int) bool { return ns[i].label < ns[j].label }
+
 // Represents node and edge in radix tree
 type node struct {
 	// regexp matcher for regexp nodes
@@ -88,6 +96,109 @@ type node struct {
 
 	// first byte of the prefix
 	label byte
+}
+
+func (n *node) getEdge(ntyp nodeType, label, tail byte, rexpat string) *node {
+	childs := n.children[ntyp]
+	for i := 0; i < len(childs); i++ {
+		if childs[i].label == label && childs[i].tail == tail {
+			if ntyp == ntRegexp && childs[i].prefix != rexpat {
+				continue
+			}
+			return childs[i]
+		}
+	}
+
+	return nil
+}
+
+// addChild appends the new `child` node to the tree using the `pattern` as the trie key.
+// For a URL router like chi's, we split the static, param, regexp and wildcard segments
+// into different nodes. In addition, addChild will recursively call itself until every
+// pattern segment is added to the url pattern tree as individual nodes, depending on type.
+func (n *node) addChild(child *node, prefix string) *node {
+	search := prefix
+
+	// handler leaf node added to the tree is the child.
+	// this may be overridden later down the flow
+	hn := child
+
+	// Parse next segment
+	seg := patNextSegment(search)
+
+	segType := seg.nodeType
+
+	// Add child depending on next up segment
+	switch segType {
+
+	case ntStatic:
+		// Search prefix is all static (that is, has no params in path)
+		// noop
+
+	case ntCatchAll:
+
+	default:
+		// Search prefix contains a param, regexp or wildcard
+
+		ps := seg.ps
+		pe := seg.pe
+
+		if ps == 0 {
+			// Route starts with a param
+			child.typ = segType
+
+			if segType == ntRegexp {
+				rex, err := regexp.Compile(seg.rexpat)
+				if err != nil {
+					panic(fmt.Sprintf("invalid regexp pattern '%s' in route param", seg.rexpat))
+				}
+				child.prefix = seg.rexpat
+				child.rex = rex
+			}
+
+			child.tail = seg.tail // for params, we set the tail
+
+			if pe != len(search) {
+				// add static edge for the remaining part, split the end.
+				// its not possible to have adjacent param nodes, so its certainly
+				// going to be a static node next.
+
+				// prefix require update?
+				child.prefix = search[:pe]
+
+				search = search[pe:] // advance search position
+
+				nn := &node{
+					typ:    ntStatic, // after update
+					label:  search[0],
+					prefix: search,
+				}
+				hn = child.addChild(nn, search)
+			}
+
+		} else if ps > 0 {
+			// Route has some param
+
+			// starts with a static segment
+			child.typ = ntStatic
+			child.prefix = search[:ps]
+			child.rex = nil
+
+			// add the param edge node
+			search = search[ps:]
+
+			nn := &node{
+				typ:   segType,
+				label: search[0],
+				tail:  seg.tail,
+			}
+			hn = child.addChild(nn, search)
+		}
+	}
+
+	n.children[child.typ] = append(n.children[child.typ], child)
+	n.children[child.typ].Sort()
+	return hn
 }
 
 func (n *node) setEndpoint(method methodType, pattern string, handler http.Handler) {
@@ -153,7 +264,7 @@ func patNextSegment(pattern string) segment {
 		}
 	}
 	if pe == ps {
-		panic("chi: route param closing delimiter '}' is missing")
+		panic("route param closing delimiter '}' is missing")
 	}
 
 	key := pattern[ps+1 : pe]
@@ -211,6 +322,7 @@ func (ar *ArtRouter) Insert(method methodType, pattern string, handler http.Hand
 	n := ar.root
 
 	for {
+
 		if len(search) == 0 {
 			n.setEndpoint(method, pattern, handler)
 			return n, nil
@@ -218,7 +330,47 @@ func (ar *ArtRouter) Insert(method methodType, pattern string, handler http.Hand
 
 		label := search[0]
 
+		var seg segment
+		if label == '{' || label == '*' {
+			seg = patNextSegment(search)
+		}
+
+		parent = n
+		n = n.getEdge(seg.nodeType, label, seg.tail, seg.rexpat)
+
+		if n == nil {
+			child := &node{label: label, tail: seg.tail, prefix: search}
+			hn := parent.addChild(child, search)
+			hn.setEndpoint(method, pattern, handler)
+			return hn, nil
+		}
+
+		if n.typ > ntStatic {
+			search = search[seg.pe:]
+			continue
+		}
+
+		//n.prefix compare
+		commonPrefix := longestPrefix(search, n.prefix)
+
+
 	}
 
 	return nil, nil
+}
+
+// longestPrefix finds the length of the shared prefix
+// of two strings
+func longestPrefix(k1, k2 string) int {
+	max := len(k1)
+	if l := len(k2); l < max {
+		max = l
+	}
+	var i int
+	for i = 0; i < max; i++ {
+		if k1[i] != k2[i] {
+			break
+		}
+	}
+	return i
 }
